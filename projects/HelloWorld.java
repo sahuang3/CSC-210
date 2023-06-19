fc45f25598a12c431600d786fff1fa14402c63e6
sahuang3 <89601319+sahuang3@users.noreply.github.com>
2023-06-18 23:23:46 -0700

    Add files via upload

---
+++ projects/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ *  Assignment 1
+ *  Description: This program will print the statement "Hello World!" and give a little introduction about myself. The
+ * program will print my name and my comfort foods to eat.
+ *  Name: Samson Huang
+ *  ID: 920216528
+ *  Class: CSC 210-08
+ *  Semester: Spring 2021
+ */
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // This part will print "Hello World"
+	System.out.println("Hello World!");
+	// This part will give an introduction of myself
+        System.out.println("My name is Samson.");
+        // My favorite comfort food to eat
+        System.out.println("My favorite comfort food to eat is ramen because it's easy to make and it's delicious.");
+    }
+}
