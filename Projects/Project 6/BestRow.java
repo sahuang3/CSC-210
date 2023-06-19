96cdf89303a5330a70c67890b555fb634e76a49d
sahuang3 <89601319+sahuang3@users.noreply.github.com>
2023-06-18 23:40:30 -0700

    Add files via upload

---
+++ Projects/Project 6/BestRow.java
@@ -0,0 +1,55 @@
+/*
+ *  Assignment 6
+ *  Description: This program
+ *  Name: Samson Huang
+ *  ID: 920216528
+ *  Class: CSC 210-08
+ *  Semester: Spring 2021
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+public class BestRow {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Takes the user's input to create the size of matrix
+        System.out.println("Enter the desired amount of rows, columns you want");
+        int row = input.nextInt();
+        int col = input.nextInt();
+        System.out.println("You've entered " + row + " rows");
+        System.out.println("You've entered " + col + " columns");
+
+        // Declaring and initializing matrix
+        int[][] matrix;
+        matrix = new int[row][col];
+
+        // Using for loops to collect the User's input of numbers
+        System.out.println("Enter numbers you want to input");
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                matrix[x][y] = input.nextInt();
+            }
+        }
+        // Creating a new array to store sum of each row
+        int[] sumArray = new int[matrix.length];
+
+        // Using for loop to add the sum of each row
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                sumArray[x] += matrix[x][y];
+            }
+        }
+        System.out.println(Arrays.toString(sumArray));
+
+        // Compare the sum of each row
+        int maxSum = sumArray[0];
+        int maxRowNum = 0;
+        for (int x = 0 ; x < sumArray.length; x++){
+            if (sumArray[x]>= sumArray[0]){
+                maxSum = sumArray[x];
+                maxRowNum = x+1;
+            }
+        }
+        System.out.println("The max sum is " +maxSum +" from row #" +maxRowNum);
+    }
+}
---
+++ Projects/Project 6/Pattern.java
@@ -0,0 +1,42 @@
+/*
+ *  Assignment 6
+ *  Description: This program creates a matrix based on the user's input for the size and marks the given position
+ *  with the number "1".
+ *  Name: Samson Huang
+ *  ID: 920216528
+ *  Class: CSC 210-08
+ *  Semester: Spring 2021
+ */
+import java.util.Scanner;
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Takes the user's input to create the size of matrix
+        System.out.println("Enter the desired amount of rows, columns you want");
+        int row = input.nextInt();
+        int col = input.nextInt();
+        System.out.println("You've entered " +row +" rows");
+        System.out.println("You've entered " +col +" columns");
+
+        // Takes the user's input for desired position they want to mark
+        System.out.println("Enter the desired position you want to be marked with the number: 1");
+        int rowInput = input.nextInt();
+        int colInput = input.nextInt();
+        System.out.println("You've entered position [" +rowInput + "," +colInput +"]");
+
+        // Declaring and initializing matrix
+        int [][] matrix;
+        matrix = new int[row][col];
+
+        // Using for loops to create the matrix table
+        for (int x = 0; x < matrix.length; x++){
+            for (int y = 0; y < matrix[x].length; y++){
+                System.out.print(matrix[x][y] + " ");
+            }
+            // To mark the user input, we assign [x][y] = 1
+            matrix[rowInput][colInput] = 1;
+            System.out.println();
+        }
+    }
+}
