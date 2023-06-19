7a69f92431a6b170f821db2bacd03beb396d1d3f
sahuang3 <89601319+sahuang3@users.noreply.github.com>
2023-06-18 23:30:08 -0700

    Add files via upload

---
+++ Projects/Project 3/Factorial.java
@@ -0,0 +1,32 @@
+/*
+ *  Assignment 3
+ *  Description: A program that uses the "for" and "while" loop to find the factorial given a specific number.
+ *  Name: Samson Huang
+ *  ID: 920216528
+ *  Class: CSC 210-08
+ *  Semester: Spring 2021
+ */
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Declaring the variables "i" and "factorial"
+        int i = 1, fact = 1;
+        // The user inputs a number
+        System.out.println("Please insert a number to find the factorial");
+        int number = input.nextInt();
+        // Input must be between 1 to 12
+        if (number >= 1 && number <= 12) {
+            //Using "While" loop to calculate the factorial with the given number
+            while (i <= number) {
+                fact = fact * i;
+                i++;
+            }
+            System.out.println("The factorial of " + number + " is " + fact);
+        } else {
+            System.out.println("Wrong input. Number must be between 1 to 12");
+        }
+    }
+}
---
+++ Projects/Project 3/FactorialForLoop.java
@@ -0,0 +1,31 @@
+/*
+ *  Assignment 3
+ *  Description: A program that uses the "for" and "while" loop to find the factorial given a specific number.
+ *  Name: Samson Huang
+ *  ID: 920216528
+ *  Class: CSC 210-08
+ *  Semester: Spring 2021
+ */
+
+import java.util.Scanner;
+
+public class FactorialForLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Declaring the variables "factorial"
+        int fact = 1;
+        // The user inputs a number
+        System.out.println("Please insert a number to find the factorial");
+        int number = input.nextInt();
+        // Input must be between 1 to 12
+        if (number >= 1 && number <= 12) {
+            //Using "For" loop to calculate the factorial with the given number
+             for (int i=1 ; i<=number;i++) {
+                fact = fact * i;
+            }
+            System.out.println("The factorial of " + number + " is " + fact);
+        } else {
+            System.out.println("Wrong input. Number must be between 1 to 12");
+        }
+    }
+}
